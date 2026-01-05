30a6dd1f41fb766c371902b0c60285d21e7087c3
Aakrist16 <aakristacharya47@gmail.com>
2026-01-05 10:56:53 +0545

    added week 5 file

--- week5/workshop/index.php
+++ week5/workshop/index.php
@@ -9,9 +9,11 @@
     <?php include "header.php"; ?>
 
 <ul>
+
     <li><a href="add_student.php">Add Student Info</a></li>
     <li><a href="upload.php">Upload Portfolio File</a></li>
     <li><a href="students.php">View Students</a></li>
+    
 </ul>
 
 <?php include "footer.php"; ?>
